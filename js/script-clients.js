b7390aa9a527c0530a211031521d97077d46dae5
WesleyVerpoest <s152161@ap.be>
2023-12-12 22:23:29 +0100

    Update script-clients.js
    achternaam toegevoegd

--- js/script-clients.js
+++ js/script-clients.js
@@ -39,7 +39,7 @@ fetch('https://randomuser.me/api/?results=6')
 			<img src="${user.picture.large}" class="card-img-top" alt="foto van ${user.name.first}">
 
 			<div class="card-body">
-			<p class="card-title text-uppercase fs-6 fw-bolder pt-3"> ${user.name.first} </p>
+			<p class="card-title text-uppercase fs-6 fw-bolder pt-3"> ${user.name.first} ${user.name.last} </p>
 			<p class="card-text h6 small mt-2">Ik kom uit ${user.location.country} en ben fan van de koffiebonen van ${random_item(brands)}!</p>
 			<a href="mailto:${user.email}">
 			  <i class="bi bi-envelope koffiebruin fs-3"></i>
